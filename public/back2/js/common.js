b680ccac81c2956b6a4c8c9cd3738b6890363064
rr1456686431 <1456686431@qq.com>
2018-11-28 15:25:17 +0800

    2-登录验证完成

---
+++ public/back2/js/common.js
@@ -0,0 +1,14 @@
+// 公共的js
+
+/*
+    进度条
+    NProgress.start()
+    NProgress.done()
+
+*/
+$(document).on('click', function () {
+    NProgress.start()
+})
+$(document).on('click', function () {
+    NProgress.done()
+})
--- public/back2/js/login.js
+++ public/back2/js/login.js
@@ -32,6 +32,9 @@ $(function() {
             min: 2,
             max: 6,
             message: "用户名长度必须是2-6位"
+          },
+          callback: {
+            message:'用户名不存在'
           }
         }
       },
@@ -46,6 +49,9 @@ $(function() {
             min: 6,
             max: 12,
             message: "密码长度必须是6-12位"
+          },
+          callback:{
+            message:'密码错误'
           }
         }
       }
@@ -72,14 +78,16 @@ $(function() {
       data: $('#form').serialize(),
       dataType: 'json',
       success: function( info ) {
-        console.log( info );
+        console.log( info )
         if ( info.error === 1000 ) {
-          alert("用户名不存在");
-          return;
+          // alert("用户名不存在");
+          $('#form').data('bootstrapValidator').updateStatus('username','INVALID','callback')
+          // return
         }
         if ( info.error === 1001 ) {
-          alert("密码错误")
-          return;
+          // alert("密码错误")
+          $('#form').data('bootstrapValidator').updateStatus('password','INVALID','callback')
+          // return
         }
         if ( info.success ) {
           // 登录成功
